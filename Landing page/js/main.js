37325fddbe5ce371ce0c45fbbe3384de028fc399
ad1sjkee <ad1sjkeee@gmail.com>
2019-02-07 12:00:05 +0300

    update

--- Landing page/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  $('a[href^="#"]').on('click', function(event) {
-    // отменяем стандартное действие
-    event.preventDefault();
-    var sc = $(this).attr("href"),
-        dn = $(sc).offset().top;
-    $('html, body').animate({scrollTop: dn}, 1000);
-  });
-});
