d1fd53b64f9cd35b19f13c9dfd01f72f73d30888
ad1sjkee <ad1sjkeee@gmail.com>
2019-02-07 11:35:24 +0300

    Compleate

---
+++ Landing page/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('a[href^="#"]').on('click', function(event) {
+    // отменяем стандартное действие
+    event.preventDefault();
+    var sc = $(this).attr("href"),
+        dn = $(sc).offset().top;
+    $('html, body').animate({scrollTop: dn}, 1000);
+  });
+});
